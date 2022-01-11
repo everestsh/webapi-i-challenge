dab34ef52e72a04be8b095d39557d005200e9f89
sooof <sooof@me.com>
2022-01-11 01:38:21 -0800

    test with postman

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 // implement your API here
 
 const express = require('express');
-const port = 9000;
+const port = 8000;
 const server = express();
 
-server.get('/hobbits', (req, res)=> {
-    const hobbits = [
-        {
-          id: 1,
-          name: 'Samwise Gamgee',
-        },
-        {
-          id: 2,
-          name: 'Frodo Baggins',
-        },
-      ];
+server.use(express.json())
+// server.get('/hobbits', (req, res)=> {
+//     const hobbits = [
+//         {
+//           id: 1,
+//           name: 'Samwise Gamgee',
+//         },
+//         {
+//           id: 2,
+//           name: 'Frodo Baggins',
+//         },
+//       ];
 
-    // res.send('hello world!!!!')
-    // res.status(200).send(hobbits);
-    res.status(200).json(hobbits);
-})
+//     // res.send('hello world!!!!')
+//     // res.status(200).send(hobbits);
+//     res.status(200).json(hobbits);
+// })
+
+server.get('/hobbits', (req, res)=>{
+    res.send('Welcome to Hobbiton')
+}) // READ data
+server.post('/hobbits', (req, res)=>{
+    res.status(201).json({url: "/hobbits", operation: 'POST'})
+}) // CREATE data
+server.put('/hobbits', (req, res)=>{
+    res.status(200).json({url: "/hobbits", operation: 'PUT'})
+}) // UPDATE data
+server.delete('/hobbits', (req, res)=>{
+    // res.json('test delete by endpoint')// work
+    res.sendStatus(204)
+}) // DELETE data
+
 server.listen(port, ()=> {
-    console.log('API Running on port 9000!!!')
+    console.log(`server listening  port ${port}`)
 })
 
 // 1> yarn 
