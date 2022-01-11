410b1607db6bc1ffad7e0802294de20e98bb6ab8
sooof <sooof@me.com>
2022-01-10 17:07:42 -0800

    hello world

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+
+const express = require('express');
+const port = 9000;
+const server = express();
+
+server.get('/', (req, res)=> {
+    res.send('hello world!!!!')
+})
+server.listen(port, ()=> {
+    console.log('API Running on port 9000!!!')
+})
+
+// 1> yarn 
+// 2> yarn add express
+// 3> npx gitignore node
+// 4> yarn  server  :  npm run server
