8e83a0adff39aac912352bb522b45751ef38e606
sooof <sooof@me.com>
2022-01-11 08:04:24 -0800

    use useRoutes.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // implement your API here
 
 const express = require('express')
+const useRoutes = require('./useRoutes')
 
 const server = express()
 
-server.get('/', (req, res)=> res.send('API up and running!'))
+// server.get('/', (req, res)=> res.send('API up and running!'))
+server.use('/users', useRoutes)
 
 server.listen(8000, ()=> console.log('API running on port 8000'))
---
+++ useRoutes.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.status(200).send('hello from the GET /users endpoint. ')
+})
+
+router.get('/:id', (req, res)=> {
+    const id = req.params.id
+    res.status(200).send(`hello from the GET /users/${id} endpoint`)
+})
+router.post('/', (req, res)=>{
+    res.status(201).send(`hello from the POST /users endpoint.`)
+})
+
+module.exports = router
