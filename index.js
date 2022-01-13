e43ba236ee66d02b0bae799e3159ce7929f630d1
sooof <sooof@me.com>
2022-01-12 16:32:53 -0800

    search

--- index.js
+++ index.js
@@ -1,51 +1,79 @@
 // implement your API here
 
-const express = require('express');
+const express = require("express");
 const port = 8000;
 const server = express();
 
-server.use(express.json())
-// server.get('/hobbits', (req, res)=> {
-//     const hobbits = [
-//         {
-//           id: 1,
-//           name: 'Samwise Gamgee',
-//         },
-//         {
-//           id: 2,
-//           name: 'Frodo Baggins',
-//         },
-//       ];
+server.use(express.json());
 
-//     // res.send('hello world!!!!')
-//     // res.status(200).send(hobbits);
-//     res.status(200).json(hobbits);
-// })
+server.get("/", (req, res) => {
+  res.send("Hello World");
+});
+//  write custom middleware
 
-server.get('/hobbits', (req, res)=>{
-    res.send('Welcome to Hobbiton')
-}) // READ data
-server.post('/hobbits', (req, res)=>{
-    res.status(201).json({url: "/hobbits", operation: 'POST'})
-}) // CREATE data
-server.put('/hobbits', (req, res)=>{
-    res.status(200).json({url: "/hobbits", operation: 'PUT'})
-}) // UPDATE data
-server.delete('/hobbits', (req, res)=>{
-    // res.json('test delete by endpoint')// work
-    res.sendStatus(204)
-}) // DELETE data
 
-server.listen(port, ()=> {
-    console.log(`server listening  port ${port}`)
-})
+//http://localhost:8000/hobbits?sortby=name
+server.get("/hobbits", (req, res) => {
+  const sortField = req.query.sortby || "id";
+  const hobbits = [
+    {
+      id: 1,
+      name: "Samwise Gamgee",
+    },
+    {
+      id: 3,
+      name: "Bilbo Baggins",
+    },
+    {
+      id: 2,
+      name: "Frodo Baggins",
+    },
+  ];
 
-// 1> yarn 
+  // res.send('hello world!!!!')
+  // res.status(200).send(hobbits);
+  
+  const response = hobbits.sort((a, b) => 
+   ( a[sortField] < b[sortField] ? -1 : +1)
+  );
+  res.status(200).json(hobbits);s
+});
+let hobbits = [
+  {
+    id: 1,
+    name: "Bilbo Baggins",
+    age: 111,
+  },
+  {
+    id: 2,
+    name: "Frodo Baggins",
+    age: 33,
+  },
+];
+let mextId = 3;
+
+// server.get('/hobbits', (req, res)=>{
+//     res.send('Welcome to Hobbiton')
+// }) // READ data
+server.post("/hobbits", (req, res) => {
+  res.status(201).json({ url: "/hobbits", operation: "POST" });
+}); // CREATE data
+server.put("/hobbits", (req, res) => {
+  res.status(200).json({ url: "/hobbits", operation: "PUT" });
+}); // UPDATE data
+server.delete("/hobbits", (req, res) => {
+  // res.json('test delete by endpoint')// work
+  res.sendStatus(204);
+}); // DELETE data
+
+server.listen(port, () => {
+  console.log(`server listening  port ${port}`);
+});
+
+// 1> yarn
 // 2> yarn add express
 // 3> npx gitignore node
 // 4> yarn  server  :  npm run server
-
-
 
 /** 
  * 12> del git cached
